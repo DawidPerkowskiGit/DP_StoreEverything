e118b8f4eac81747f00a796ef7db3d9572baf8d7
DawidPerkowskiGit <dtperkowski@gmail.com>
2023-07-18 11:10:30 +0200

    Added schedule to perform http request every 10 minutes to prevent Render from unloading app's resources resulting in shutting down the app

---
+++ src/main/java/com/dmrl/storeverything/schedules/KeepTheAppRunning.java
@@ -0,0 +1,32 @@
+package com.dmrl.storeverything.schedules;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+
+@Service
+public class KeepTheAppRunning {
+    @Scheduled(fixedRate = 600000, initialDelay = 600000)
+    public void keepTheAppRunning(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://dp-storeeverything.onrender.com/"))
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+        }
+        catch (Exception e) {
+            System.out.println("Failed to perform scheduled task. Exception: "+ e);
+        }
+        System.out.println("Successfully kept the app from un-allocating resources. Time: " + localDateTime.toString());
+    }
+}
